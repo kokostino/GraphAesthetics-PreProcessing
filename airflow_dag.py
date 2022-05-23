ee7e193dc9261d820963eb2e3b16abf6314b2eb2
kokostino <56522863+kokostino@users.noreply.github.com>
2022-05-23 20:47:02 +0200

    add pca

--- airflow_dag.py
+++ airflow_dag.py
@@ -6,7 +6,9 @@ from airflow.utils.dates import days_ago
 
 from colour_preprocessing import get_colour_names_of_all_images
 from feature_extraction import feature_vectors_of_folder
+from dim_reduce import get_reduced_features
 
+# ------- set function variables -----------
 
 def colour_values():
     return {
@@ -15,6 +17,19 @@ def colour_values():
         "dict_file": "colourNames.csv",
         "n_clstrs": 20
     }
+
+def feature_extraction_values():
+    return {
+        'folder': '/GitHub/GraphAesthetics-PreProcessing/investigating-aesthetics'
+    }
+
+def pca_values():
+    return {
+            'zahl': 20,
+            'metafolder': "/GitHub/GraphAesthetics-PreProcessing/investigating-aesthetics_metadata",
+            'file': "feature_vectors.csv"
+    }
+# -----------------------------------------------------
 
 default_args = {
     'owner': 'Britney Spears',
@@ -25,6 +40,8 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
+
+# -----------------------------------------------------
 
 with DAG(
     dag_id='Image_Processing',
@@ -44,13 +61,30 @@ with DAG(
         op_kwargs=colour_values_task.output
     )
 
+    fv_values_task = PythonOperator(
+        task_id="fv_values",
+        python_callable=feature_extraction_values
+    )
+
     extract_feature_vectors = PythonOperator(
         task_id="extract_feature_vectors",
         python_callable=feature_vectors_of_folder,
-        op_kwargs={
-            'folder': '/GitHub/GraphAesthetics-PreProcessing/investigating-aesthetics'
-    }
+        op_kwargs=fv_values_task.output
+    )
+
+    reduce_task = PythonOperator(
+        task_id="reduce_values",
+        python_callable=pca_values
+    )
+
+    reduce_feature_vectors = PythonOperator(
+        task_id="reduce_feature_vectors",
+        python_callable=get_reduced_features,
+        op_kwargs=reduce_task.output
     )
 
 
     colour_values_task>>extract_colour_info
+    fv_values_task>>extract_feature_vectors>>reduce_task>>reduce_feature_vectors
+
+# -----------------------------------------------------
